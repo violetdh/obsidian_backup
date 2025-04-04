4fffd963c80edfe95f89bdaf9084762f82fec7e9
violet <violetalt7@gmail.com>
2025-04-04 21:52:27 +0000

    date sorting

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 this is a tool I'm writing in python to take a folder and back it up daily, with a history going back 7 days and maybe a monthly copy too, not sure yet. 
 
-it is intended for use with syncthing and my obsidian vault. basically, it will take the folder my vault is synced to and store backups of it daily that aren't influenced by syncthing, so that if something happens to the vault it won't be unrecoverable due to being synchronised.
+it takes a specified src folder and copies it to a destination folder, renaming it to the current date. it will keep up to 7 of these in the folder at a time.
--- backup.py
+++ backup.py
@@ -7,9 +7,14 @@ backup_dir = "../backup_test/target/"
 
 def copy():
     date = dt.now().date().strftime("%d-%m-%Y") # gets current date as string
-    if len(os.listdir(backup_dir)) >= 7: # checks if 7 or more backups exist
+    # converts date strings in filenames to date objects
+    dates = [dt.strptime(ts, "%d-%m-%Y") for ts in os.listdir(backup_dir)]
+    dates.sort() # sorts them
+    backups = [dt.strftime(ts, "%d-%m-%Y") for ts in dates] # converts them back to strings
+    print(backups)
+    if len(backups) >= 7: # checks if 7 or more backups exist
         shutil.rmtree(backup_dir+backups[6]) # deletes the 7th
     shutil.copytree(src, backup_dir+date) # copies src folder to target folder as current date 
-    print("backup " + date + "done!")
+    print("backup " + date + " done!")
 
 copy()
