90b431e550f8e4a574fab682e9e77525329017e6
violet <violetalt7@gmail.com>
2025-04-04 21:24:41 +0000

    basic functionality done

--- backup.py
+++ backup.py
@@ -1,9 +1,15 @@
-import os
 import shutil
-import schedule
-import time
+import os
+from datetime import datetime as dt
 
-src = ""
-backup_dir = ""
+src = "../backup_test/source"
+backup_dir = "../backup_test/target/"
 
-backups = []
+def copy():
+    date = dt.now().date().strftime("%d-%m-%Y") # gets current date as string
+    if len(os.listdir(backup_dir)) >= 7: # checks if 7 or more backups exist
+        shutil.rmtree(backup_dir+backups[6]) # deletes the 7th
+    shutil.copytree(src, backup_dir+date) # copies src folder to target folder as current date 
+    print("backup " + date + "done!")
+
+copy()
